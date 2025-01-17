cde2e580287eb078419f6406ff1d49dee47a543f
HDB <chcctv2@outlook.com>
2025-01-17 22:49:55 +0800

    TCP V2.0.2
    修復更新BUG

---
+++ TCP V2.0.2/python dll/TCP dll.py
@@ -0,0 +1,17 @@
+import socket
+
+def main():
+    host = ip  # 例如 '192.168.1.100'
+    port = 11111
+
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((host, port))
+
+    while True:
+        message = txt
+        client_socket.send(message.encode('utf-8'))
+        response = client_socket.recv(1024).decode('utf-8')
+        print(f"{response}")
+
+if __name__ == "__main__":
+    main()
